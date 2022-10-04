3408b4491af83bb23246ecab71c458fc8e71767f
Italo Guglielmini <italo.guglielmini@gmail.com>
2022-10-03 23:04:24 -0300

    feat: initial project

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+// import { Container } from './styles';
+
+const Header: React.FC = () => {
+  return (
+    <View>
+        <Text>Home</Text>
+    </View>
+  );
+}
+
+export default Header;
